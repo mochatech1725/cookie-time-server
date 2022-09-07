105d2b15992e586dd3c6834ad936cc5b6a9db626
TeialD <teial.starks@gmail.com>
2022-09-07 12:52:25 -0700

    Correc Schema name

--- src/models/product-inventory-model.js
+++ src/models/product-inventory-model.js
@@ -2,10 +2,9 @@ import  mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 import { ProductInventoryItemSchema } from './product-inventory-item-model.js';
 
-const ProductInventoryItemSchema = new Schema({
+const ProductInventorySchema = new Schema({
     campaign_id: { type: Schema.Types.ObjectId, required: true },
     inventory_items: [ProductInventoryItemSchema],
-    quantity: { type: Number, required: true },
     created_at: {type: Date, default: Date.now}
   },
   {collection: 'product-inventory'}
